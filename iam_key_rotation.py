3b491d830ce5eeef6d4163e082972d9922e0c7bd
JasK-Git <jasleen.notay@yahoo.com>
2026-06-28 22:00:02 +0530

    updating logger commands

--- iam_key_rotation.py
+++ iam_key_rotation.py
@@ -4,7 +4,8 @@ import os                   # To read environment variables
 from datetime import datetime, timezone             # To interpret dates
 
 # Set logging level for production code
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Query environment variable
 key_age = int(os.environ.get('EXPIRY_DAYS', 2))
@@ -15,13 +16,13 @@ iam_client = boto3.client('iam')
 # Step 1. Get all IAM users
 def get_users():
     response = iam_client.list_users()          # Return type: dict
-    logging.debug(response)
+    logger.debug(response)
 
     # 2. We need to extract IAM user and access key from the above response
     user_names = []
     for user in response.get('Users', []):
         user_names.append(user.get('UserName'))
-        logging.info(user_names)
+        logger.info(user_names)
         #print(type(user_names))
     return user_names
 
@@ -33,7 +34,7 @@ def get_access_keys(user_names):
         access_key = iam_client.list_access_keys(
             UserName=un
         )
-        logging.debug(access_key)
+        logger.debug(access_key)
 
         for metadata in access_key.get('AccessKeyMetadata', []):
             user_name = metadata.get('UserName')
@@ -42,9 +43,9 @@ def get_access_keys(user_names):
             # Calculate age of the access key
             current_date = datetime.now(timezone.utc)
             age = current_date - date
-            logging.debug(age)
+            logger.debug(age)
             user_key_date.append((user_name, ak_id, age.days))
-    logging.debug(user_key_date)
+    logger.debug(user_key_date)
     #print(type(user_key_date))
     return user_key_date
 
@@ -54,14 +55,14 @@ def delete_key(user_key_date):
     for u in user_key_date:
         try:
             if u[2] > key_age:
-                logging.info("Deleting access key {} for user {}".format(u[0], u[1]))
+                logger.info("Deleting access key {} for user {}".format(u[0], u[1]))
                 key_delete = iam_client.delete_access_key(
                     UserName=u[0],
                     AccessKeyId=u[1]
                 )
-                logging.info("Successfully deleted access key {} for user {}".format(u[0], u[1]))
+                logger.info("Successfully deleted access key {} for user {}".format(u[0], u[1]))
         except Exception as e:
-            logging.error(str(e))
+            logger.error(str(e))
     return ()
 
 def run():
