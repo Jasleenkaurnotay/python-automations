59a728302cbfd4be12cfec998af7d16a24f1df77
JasK-Git <jasleen.notay@yahoo.com>
2026-06-29 23:33:23 +0530

    Adding secret creation steps

--- iam_key_rotation.py
+++ iam_key_rotation.py
@@ -2,6 +2,7 @@ import boto3                # To speak with AWS APIs
 import logging              # Used for Log formatting
 import os                   # To read environment variables
 from datetime import datetime, timezone             # To interpret dates
+import json
 
 # Set logging level for production code
 logger = logging.getLogger()
@@ -20,7 +21,7 @@ def get_users():
 
     # 2. We need to extract IAM user and access key from the above response
     user_names = []
-    for user in response.get('Users', []):
+    for user in response.get('Users', []):          # Fallback is empty list [] because we are iterating over a lsit of dict
         user_names.append(user.get('UserName'))
         logger.info(user_names)
         #print(type(user_names))
@@ -44,7 +45,7 @@ def get_access_keys(user_names):
             current_date = datetime.now(timezone.utc)
             age = current_date - date
             logger.debug(age)
-            user_key_date.append((user_name, ak_id, age.days))
+            user_key_date.append((user_name, ak_id, age.days))          # Storing values not as a list, but tuple
     logger.debug(user_key_date)
     #print(type(user_key_date))
     return user_key_date
@@ -65,10 +66,60 @@ def delete_key(user_key_date):
             logger.error(str(e))
     return ()
 
+
+# 5. Create new Access key + Secret Access key
+def new_access_key(user_key_date):         # This function utilizes the user names list generated in the tuple because access keys for only applicable users need to be rotated
+    updated_user_keypair = []
+    for u in user_key_date:
+        try:
+            if u[2] > key_age:          # Only expired keys would be rotated
+                ak = iam_client.create_access_key(
+                    UserName=u[0]
+                )
+                logger.info("Access key and secret access key created successfully for user: {}".format(u[0]))
+                updated_user_keypair.append((u[0], ak.get('AccessKey', {}).get('AccessKeyId'), ak.get('AccessKey', {}).get('SecretAccessKey')))
+                # fallback in the above line is not an empty list but {} because we arent iterating over a list of dicts in this scenario
+        except Exception as e:
+            logger.error(str(e))
+    return updated_user_keypair
+
+## Initialize the AWS Secrets Manager Client
+sm_client = boto3.client("secretsmanager", region_name="us-east-1")
+
+# 6. Store in Secrets manager key
+def store_aws_credentials(updated_user_keypair):
+    for uuk in updated_user_keypair:
+        #Structure credentials inside a dictionary
+        credential_data = {
+            "aws_access_key_id" : uuk[1],
+            "aws_secret_access_key" : uuk[2]
+        }
+
+        try:
+            #Convert dictionary to a JSON string for SecretString
+            secret_key_pair = sm_client.create_secret(
+                Name="rotated_key_pair_{}".format(uuk[0]),
+                Description="IAM user access keys",
+                SecretString=json.dumps(credential_data)
+            )
+            logger.info("Successfully created secret in Secrets manager")
+        except Exception as e:
+            logger.error(str(e))
+            raise
+    return ()
+
+
 def run():
     users = get_users()
     key_tuple = get_access_keys(users)
-    delete_key(key_tuple)
+    uukp = new_access_key(key_tuple)
+    try:
+        cs = store_aws_credentials(uukp)
+    except Exception as e:
+        logger.error(str(e))
+        raise
+    else:
+        delete_key(key_tuple)       # Delete keys only if storing new keys is successful
 
 
 def lambda_handler(event, context):
